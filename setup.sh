336bab8003173353b1343ab5efc2dcf8395f57b2
unlikeghost <unlikeghost@protonmail.com>
2021-01-02 12:26:04 -0700

    ene 2

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ virtualenv env
 source env/bin/activate
 pip3 install -r requirements.txt
 chmod +x main.py
-printf '[Desktop Entry]\nVersion=1.0\nName=Password-Manager\nExec=./main.py\nIcon=src/img/icon.png\nType=Aplication' > password-manager.desktop
 printf 'source env/bin/activate\n python main.py' password-manager.sh
+chmod +x password-manager.sh
