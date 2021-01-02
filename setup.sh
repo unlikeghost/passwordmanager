b98967806c6b09763d7c6e89ab90b3d4490e4d3a
unlikeghost <unlikeghost@protonmail.com>
2021-01-02 13:11:18 -0700

    ene 2

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo virtualenv env
 source env/bin/activate
 pip3 install -r requirements.txt
 chmod +x main.py
-sudo printf 'cd passmanager\nsource env/bin/activate\npython main.py' > pssmanager.sh
+sudo printf '#!/bin/bash\ncd /usr/local/bin/passmanager\nsource env/bin/activate\npython main.py' > pssmanager.sh
 sudo mv pssmanager.sh /usr/local/bin
 sudo chown root: /usr/local/bin/pssmanager.sh
 sudo chmod 755 /usr/local/bin/pssmanager.sh
