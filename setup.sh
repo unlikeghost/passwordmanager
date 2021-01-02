e95737254fa1e9d9dd735b9be97a9fcdccadb4f1
unlikeghost <unlikeghost@protonmail.com>
2021-01-02 13:03:07 -0700

    ene 2

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 pip3 install virtualenv
-virtualenv env
+sudo mkdir /usr/local/bin/passmanager
+sudo mv *.py *.txt *md *.sh /usr/local/bin/passmanager
+cd /usr/local/bin/passmanager
+sudo virtualenv env
 source env/bin/activate
 pip3 install -r requirements.txt
-chmod +x main.py
-printf 'source env/bin/activate\n python main.py' password-manager.sh
-sudo mv ~/myscript.sh /usr/local/bin/SCRIPTNAME
+chmod +x consider upgrading via the '/usr/local/bin/passmanager/env/bin/python -m pip install --upgrade pip' command.main.py
+sudo printf 'cd passmanager\nsource env/bin/activate\n python main.py' > pssmanager.sh
+sudo mv pssmanager.sh /usr/local/bin
+sudo chmod +x pssmanager.sh
